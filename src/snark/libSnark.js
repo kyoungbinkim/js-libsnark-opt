7d3c8e3cecfeffcf4cf59add785eb492e49200fe
kim <kyoungbin1996@gmail.com>
2023-12-25 20:52:05 +0900

    TODO : impl libSnark.js

---
+++ src/snark/libSnark.js
@@ -0,0 +1,8 @@
+import fs from 'fs';
+import config from '../config';
+import { SnarkLib, SnarkLibUtils } from './libsnark.interface';
+import snarkInputClass from './makeSnarkInput';
+
+export class LIBSANRK extends snarkInputClass {
+    // TODO
+}
--- src/test/input.test.js
+++ src/test/input.test.js
@@ -1,7 +1,17 @@
-import snarkInputClass from "../snark/makeSnarkInput"
+import snarkInputClass, {isHex} from "../snark/makeSnarkInput"
 
 describe("INPUT TEST", () => {
-    it("test1 ", () => {
+    it("HEX test", () => {
+        console.log("0x1234\t: ", isHex("0x1234"))
+        console.log("1234\t: ", isHex("1234"))
+        console.log("ff1a234\t: ", isHex("ff1a234"))
+    })
 
+    it("test1 ", () => {
+        const REGI_INPUT = new snarkInputClass('regi');
+        REGI_INPUT.add('age', ['1234', 'aabb']);
+        REGI_INPUT.add('name', 'aaff1234');
+        REGI_INPUT.check()
+        console.log(REGI_INPUT.toJSON())
     })
 })
