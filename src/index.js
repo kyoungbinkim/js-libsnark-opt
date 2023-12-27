6827770f21e1c1f5b20d84d76d034a119051a71a
kim <kyoungbin1996@gmail.com>
2023-12-27 13:43:36 +0900

    impl : clean

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+import fs from 'fs';
+import path from 'path';
 import process from 'process';
 import initConfig from './initConfig';
+import config from './config';
 import { compileSnark, copySnark, testSnark } from './snark/compileSnark';
 
 if(process.argv.length < 3){
@@ -28,5 +31,13 @@ switch(process.argv[2]){
             process.exit(1);
         }
         testSnark(process.argv[3]);
+        break;
+    
+    case "clean":
+        fs.readdirSync(config.LIBSNARK_PATH).forEach(f => {
+            if (path.extname(f) == ".json"){
+                fs.unlink(config.LIBSNARK_PATH + f, (err) => {if (err) console.log(err)})
+            }
+        })
         break;
 }
